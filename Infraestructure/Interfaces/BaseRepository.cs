5cdc10217e4137c1908f1c1480649e5c80f333f8
BitZ <85895666+iArtie@users.noreply.github.com>
2021-12-02 10:24:54 -0600

    Add files via upload

---
+++ Infraestructure/Interfaces/BaseRepository.cs
@@ -0,0 +1,48 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure
+{
+	public abstract class BaseRepository<T> : IModel<T>
+	{
+		public List<T> datos;
+		protected BaseRepository()
+		{
+			datos = new List<T>();
+		}
+		public void Create(T t)
+		{
+			if (t == null)
+			{
+				throw new ArgumentException("El valor de lo que ingreso es nulo");
+			}
+			datos.Add(t);
+		}
+
+		public void Delele(T t)
+		{
+			datos.Remove(t);
+		}
+
+		public int GetLastId()
+		{
+			try
+			{
+				return datos.Count == 0 ? 0 : (int)datos[datos.Count - 1].GetType().GetProperty("Id").GetValue(datos[datos.Count - 1]);
+			}
+			catch (Exception)
+			{
+				throw new ArgumentException("El objeto no posee la propiedad Id");
+			}
+		}
+
+		public ICollection<T> Read()
+		{
+			return datos;
+		}
+	}
+}
---
+++ Infraestructure/Interfaces/NotaListRepository.cs
@@ -0,0 +1,57 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Interfaces
+{
+    public class NotaListRepository : BaseRepository<Nota>, INotaRepository
+    {
+        public int CalcularNotas(int primer, int segundo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CalcularPromedio()
+        {
+            decimal promedio = datos.Sum(x => x.NotaFinal);
+            return Convert.ToInt32(promedio/6);
+        }
+
+        public Estudiante EstudianteById(int Id)
+        {
+            for(int i=0; i < datos.Count; i++)
+            {
+                if (Id == datos[i].Estudiante.Id)
+                {
+                    return datos[i].Estudiante;
+                }
+            }
+            return null;
+        }
+
+        public ICollection<Asignatura> GetAsignaturas()
+        {
+
+            List<Asignatura> asignaturas = new List<Asignatura>();
+            asignaturas =(List<Asignatura>) datos.Select(x => x.Asignaturas);
+            return asignaturas;
+            
+        }
+
+        public ICollection<Nota> MejoresEstudiantes()
+        {
+            List<Nota> MejoresNotas = new List<Nota>();
+            datos.OrderBy(x => x.NotaFinal);
+            for(int i=0; i<2; i++)
+            {
+                MejoresNotas.Add(datos[i]);
+            }
+            return MejoresNotas;
+        }
+
+    }
+}
