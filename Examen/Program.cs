056424acca58a94a06f4f65a0576b2ea7035bca2
BitZ <85895666+iArtie@users.noreply.github.com>
2021-12-02 10:25:57 -0600

    Add files via upload

---
+++ Examen/Program.cs
@@ -0,0 +1,32 @@
+﻿using AppCore.Interfaces;
+using AppCore.Services;
+using Autofac;
+using Domain.Interfaces;
+using Infraestructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+		Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			var builder = new ContainerBuilder();
+
+			builder.RegisterType<NotaListRepository>().As<INotaRepository>();
+			builder.RegisterType<NotaServices>().As<NotaRepositoryServices>();
+			var container = builder.Build();
+			Application.Run(new Form1(container.Resolve<NotaRepositoryServices>()));
+		}
+	}
+}
